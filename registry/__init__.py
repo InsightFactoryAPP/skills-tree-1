824648ccc7b4e96cc362675ac1e44992cf19bd4e
Ossama Hashim <samo.hossam@gmail.com>
2026-09-19 20:41:15 +0300

    phase2: export typed capability runtime

--- registry/__init__.py
+++ registry/__init__.py
@@ -1,7 +1,8 @@
 """Universal registry runtime package."""
 
+from .capability import CapabilityRuntime
 from .compatibility import CompatibilityRuntime
 from .evidence import EvidenceRuntime
 from .runtime import UniversalRegistry
 
-__all__ = ["CompatibilityRuntime", "EvidenceRuntime", "UniversalRegistry"]
+__all__ = ["CapabilityRuntime", "CompatibilityRuntime", "EvidenceRuntime", "UniversalRegistry"]
